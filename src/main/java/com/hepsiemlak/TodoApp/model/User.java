d5c1270a14b88c00d81548e3ea849bceacc38594
Selman Topal <selmant.20f@gmail.com>
2023-12-05 23:31:16 +0300

    created models

---
+++ src/main/java/com/hepsiemlak/TodoApp/model/User.java
@@ -0,0 +1,36 @@
+package com.hepsiemlak.TodoApp.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.couchbase.core.mapping.Document;
+import org.springframework.data.couchbase.core.mapping.Field;
+import org.springframework.data.couchbase.core.mapping.id.GeneratedValue;
+import org.springframework.data.couchbase.core.mapping.id.GenerationStrategy;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+public class User {
+  @Id
+  @EqualsAndHashCode.Include
+  @GeneratedValue(strategy = GenerationStrategy.UNIQUE)
+  String id;
+  @Field
+  String name;
+  @Field
+  String email;
+  @Field
+  @ToString.Exclude
+  String password;
+}
