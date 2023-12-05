d54a328d3863e53732853f61a99ccb015360ea2d
Selman Topal <selmant.20f@gmail.com>
2023-12-05 23:28:38 +0300

    Initial commit and added database configuration

---
+++ src/main/java/com/hepsiemlak/TodoApp/TodoAppApplication.java
@@ -0,0 +1,12 @@
+package com.hepsiemlak.TodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class TodoAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppApplication.class, args);
+	}
+}
